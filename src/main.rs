ef24152b0a4bbeecacf0e67bef89363d8c9a106d
Ryan Chen <ryanjchen2@gmail.com>
2023-11-30 23:26:16 -0800

    donezo

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 use graphicsmagick::{initialize, types::FilterTypes, types::GravityType, wand::MagickWand};
 use std::path::PathBuf;
-
-fn make_square(filename: &str) -> Result<&str, &str> {
+use anyhow::Context;
+fn make_square(filename: &str) -> anyhow::Result<()> {
     let path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("yeet.jpg");
     let path = path.to_str().context("get image path failed")?;
 
-    let mut mw = MagickWand::new()
+    initialize();
+
+    let mut binding = MagickWand::new();
+    let mut mw = binding
         .read_image(path)?
-        .transform_image("", "2000x2000>")?
-        .set_gravity(graphicsmagick::types::GravityType::CenterGravity)?
-        .extent_image(2200, 2200, -100, -100)?
-        .write_image("square.jpg")?;
+        .set_image_gravity(graphicsmagick::types::GravityType::CenterGravity)?
+        .transform_image("", "2000x2000>").expect("yeet");
+
+    if mw.get_image_height() == 2000 {
+        let x = &mw.get_image_width() / 2;
+        mw.extent_image(2200, 2200, (1100 - x).try_into().unwrap(), 100)?;
+    } else {
+        let y = &mw.get_image_height() / 2;
+        mw.extent_image(2200, 2200, 100, (1100 - y).try_into().unwrap())?;
+    }
+
+    mw.write_image("square2.jpg")?;
 
     //let mut wand = MagickWand::new();
     //wand.read_image(filename).unwrap();
@@ -19,8 +30,7 @@ fn make_square(filename: &str) -> Result<&str, &str> {
     //wand.crop_image(2200, 2200, 0, 0);
 
     //wand.write_image("square.jpeg");
-
-    Ok("yeet")
+    Ok(())
 }
 fn main() {
     make_square("yeet.jpg");
