78be16374335b09ce6bf6a77911e85dcb72380fe
Ryan Chen <ryanjchen2@gmail.com>
2023-12-01 19:57:49 -0800

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use graphicsmagick::{initialize, types::FilterTypes, types::GravityType, wand::MagickWand};
 use std::path::PathBuf;
 use anyhow::Context;
+
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+
 fn make_square(filename: &str) -> anyhow::Result<()> {
     let path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("yeet.jpg");
     let path = path.to_str().context("get image path failed")?;
@@ -26,6 +34,23 @@ fn make_square(filename: &str) -> anyhow::Result<()> {
     mw.write_image(format!("{}-square.jpg", s))?;
     Ok(())
 }
-fn main() {
-    let _ = make_square("yeet.jpg");
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3128").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello, World!"
 }
