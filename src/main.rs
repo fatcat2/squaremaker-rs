8761a8761ba854db02803c9a18c102491622aab3
Ryan Chen <ryanjchen2@gmail.com>
2024-01-13 08:30:15 -0800

    yeet'

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-use graphicsmagick::{initialize, types::FilterTypes, types::GravityType, wand::MagickWand};
-use std::path::PathBuf;
 use anyhow::Context;
+use core::slice::SlicePattern;
+use graphicsmagick::{
+    initialize, types::FilterTypes, types::GravityType, wand::MagickWand, MagickBoxSlice,
+};
+use std::path::PathBuf;
 
 use axum::{
-    routing::{get, post},
-    http::StatusCode,
     extract::{DefaultBodyLimit, Multipart},
-    Json, Router, response::Html,
+    http::StatusCode,
+    response::Html,
+    routing::{get, post},
+    Json, Router,
 };
 use serde::{Deserialize, Serialize};
 
@@ -20,7 +24,8 @@ fn make_square(filename: &str) -> anyhow::Result<()> {
     let mut mw = binding
         .read_image(path)?
         .set_image_gravity(graphicsmagick::types::GravityType::CenterGravity)?
-        .transform_image("", "2000x2000>").expect("yeet");
+        .transform_image("", "2000x2000>")
+        .expect("yeet");
 
     if mw.get_image_height() == 2000 {
         let x = &mw.get_image_width() / 2;
@@ -36,7 +41,42 @@ fn make_square(filename: &str) -> anyhow::Result<()> {
     Ok(())
 }
 
+fn make_square_from_blob(blob: Vec<u8>) -> Vec<u8> {
+    let path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("yeet.jpg");
+    let path = path.to_str().context("get image path failed")?;
+
+    initialize();
+
+    let mut binding = MagickWand::new();
+    let mut mw = binding
+        .read_image_blob(&blob)
+        .unwrap()
+        .set_image_gravity(graphicsmagick::types::GravityType::CenterGravity)
+        .unwrap()
+        .transform_image("", "2000x2000>")
+        .expect("yeet");
+
+    if mw.get_image_height() == 2000 {
+        let x = &mw.get_image_width() / 2;
+        mw.extent_image(2200, 2200, (1100 - x).try_into().unwrap(), 100)?;
+    } else {
+        let y = &mw.get_image_height() / 2;
+        mw.extent_image(2200, 2200, 100, (1100 - y).try_into().unwrap())?;
+    }
+
+    mw.set_image_format("jpg")
+        .unwrap()
+        .write_image_blob()
+        .unwrap()
+        .to_vec()
+}
+
 async fn make_square_handler(mut multipart: Multipart) {
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        if field.name().unwrap().to_string().eq("file") {
+            let data = field.bytes().await.unwrap();
+        }
+    }
 }
 
 #[tokio::main]
@@ -55,7 +95,7 @@ async fn main() {
 }
 
 // basic handler that responds with a static string
-async fn root() -> Html<String>{
+async fn root() -> Html<String> {
     let html_text = std::fs::read_to_string("index.html").unwrap();
     Html(html_text)
 }
